e7461b53be48b1f17f6b768681a2bd4522747182
whyndson <57191892+whyndson@users.noreply.github.com>
2019-11-14 16:31:35 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,21 +3,37 @@
 <head>
 	<title>Projeto PWEB-TEIN</title>
 	<meta charset="utf-8">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
 <body>
-	<input type="text" name="nome" placeholder="Nome"><br><br>
-	<input type="number" name="idade" placeholder="Idade"><br>
-	<input type="text" name="peso" placeholder="Peso"><br>
-	<input type="text" name="altura" placeholder="Altura"><br>
-	<input type="text" name="altura" placeholder="Curso">
-	<p>Facilidade com questões de lógica</p>
-	<input type="checkbox" name="checkbox-baixo"> Baixo<br>
-	<input type="checkbox" name="checkbox-medio"> Médio<br>
-	<input type= "checkbox" name="checkbox-alto"> Alto<br>
-	<p>Frequência cardíaca</p>
-	<input type="text" name="frequencia-cardiaca" placeholder="Frequência cardíaca">
-	<br>
-	<br>
-	<a href="../view/tela1.php">Inicar teste</a>
+	<form id="form">
+		<input id="nome" type="text" name="nome" placeholder="Nome"><br><br>
+		<input id="idade" type="number" name="idade" placeholder="Idade"><br>
+		<input type="text" name="peso" placeholder="Peso"><br>
+		<input type="text" name="altura" placeholder="Altura"><br>
+		<input type="text" name="altura" placeholder="Curso">
+		<p>Facilidade com questões de lógica</p>
+		<input type="checkbox" name="checkbox-baixo"> Baixo<br>
+		<input type="checkbox" name="checkbox-medio"> Médio<br>
+		<input type= "checkbox" name="checkbox-alto"> Alto<br>
+		<p>Frequência cardíaca</p>
+		<input type="text" name="frequencia-cardiaca" placeholder="Frequência cardíaca">
+		<br>
+		<br>
+		<input type="submit">
+	</form>
 </body>
+
+<script>
+	$("form").submit(function(event){
+		event.preventDefault();
+
+		var nome = $("#nome").val();
+		var idade = $("#idade").val();
+		var peso = $("#peso").val();
+
+		alert("lucas");
+	});
+</script>
+
 </html>
