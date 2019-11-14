30c93cb4a91afa84acbd950b6830c3dfea82643a
whyndson <57191892+whyndson@users.noreply.github.com>
2019-11-14 14:40:23 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Projeto PWEB-TEIN</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<input type="number" name="idade" placeholder="Idade"><br>
+	<input type="text" name="peso" placeholder="Peso"><br>
+	<input type="text" name="altura" placeholder="Altura"><br>
+	<input type="text" name="altura" placeholder="Curso">
+	<p>Facilidade com questões de lógica</p>
+	<input type="checkbox" name="checkbox-baixo"> Baixo<br>
+	<input type="checkbox" name="checkbox-medio"> Médio<br>
+	<input type="checkbox" name="checkbox-alto"> Alto<br>
+	<p>Frequência cardíaca</p><br>
+	<input type="text" name="frequencia-cardiaca" placeholder="Frequência cardíaca">
+	</body>
+	</html>
