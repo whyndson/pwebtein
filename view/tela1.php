4cbe0855518e6c207c70dd078e533df029dda71d
whyndson <57191892+whyndson@users.noreply.github.com>
2019-11-14 15:58:32 -0300

    Add files via upload

---
+++ view/tela1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Teste</title>
+	</head>
+	<body>
+		<form action="/pagina-processa-dados-do-form" method="post">
+		    <div>
+		        <label for="cubo">Qual dos cubos abaixo poderia ser a rotação do cubo acima?</label> <br>
+		        <button><input type="image" src="cubo1.webp" class="botao" width="200px" ></button>	
+		        <label for="resposta1"> </label> <br>
+		    </div>
+		    <div>
+		        <label for="cubo"></label>
+		    </div>
+		    
+		</form>
+	</body>
+</html>
