e42fe2993fc98c93fbcfb24117a3d601fd7ccef9
karolinemagal <46010185+karolinemagal@users.noreply.github.com>
2019-11-14 16:34:38 -0300

    Update tela1.php

--- view/tela1.php
+++ view/tela1.php
@@ -6,14 +6,32 @@
 	<body>
 		<form action="/pagina-processa-dados-do-form" method="post">
 		    <div>
+		        <button><input type="image" src="../img/cubo1.webp" class="botao" width="200px" ></button> <br>	
 		        <label for="cubo">Qual dos cubos abaixo poderia ser a rotação do cubo acima?</label> <br>
-		        <button><input type="image" src="cubo1.webp" class="botao" width="200px" ></button>	
 		        <label for="resposta1"> </label> <br>
 		    </div>
 		    <div>
-		        <label for="cubo"></label>
+		        <button> <input type="image" src="../img/cubo2.webp" class="botao" width="100px" > </button> 
+		        <button> <input type="image" src="../img/cubo3.webp" class="botao" width="100px" > </button> 
+		        <button> <input type="image" src="../img/cubo4.webp" class="botao" width="100px" > </button> 
+		        <button> <input type="image" src="../img/cubo5.webp" class="botao" width="100px" > </button>
 		    </div>
-		    
+		    <hr>
+		    <br>
+		    <br>
+		    <br>
+		    <div>
+		        <button><input type="image" src="../img/calendario.webp" class="botao" width="200px" ></button> <br>	
+		        <label for="cubo">Se o dia depois de amanhã é dois dias antes da quinta-feira, que dia é hoje?</label> <br>
+		        <!--<label for="resposta1"> </label> <br>-->
+		        <br>
+		        <button> <input type="text" placeholder="Quarta"> </button> <br>
+		       	<button> <input type="text" placeholder="Segunda"> </button> <br>
+		       	<button> <input type="text" placeholder="Terça"> </button> <br>
+		       	<button> <input type="text" placeholder="Domingo"> </button> <br>
+
+		    </div>
+
 		</form>
 	</body>
 </html>
