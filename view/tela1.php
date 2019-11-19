752d9177e7a1cbdc8902f7c3a1c87886f4dd1369
karolinemagal <46010185+karolinemagal@users.noreply.github.com>
2019-11-19 16:28:39 -0300

    Update tela1.php

--- view/tela1.php
+++ view/tela1.php
@@ -29,7 +29,35 @@
 		       	<button> <input type="text" placeholder="Segunda"> </button> <br>
 		       	<button> <input type="text" placeholder="Terça"> </button> <br>
 		       	<button> <input type="text" placeholder="Domingo"> </button> <br>
+		    </div>
+		    <hr>
+		    <br>
+		    <br>
+		    <br>
+		    <div>
+		        <button><input type="image" src="../img/cidade.webp" class="botao" width="200px" ></button> <br>	
+		        <label for="cubo">Marque a palavra que não combina com as outras</label> <br>
+		        <!--<label for="resposta1"> </label> <br>-->
+		        <br>
+		        <button> <input type="text" placeholder="Cuiabá"> </button> <br>
+		       	<button> <input type="text" placeholder="Cairo"> </button> <br>
+		       	<button> <input type="text" placeholder="Buenos Aires"> </button> <br>
+		       	<button> <input type="text" placeholder="Campinas"> </button> <br>
+		    </div>
 
+		    <hr>
+		    <br>
+		    <br>
+		    <br>
+		     <div>
+		        <button><input type="image" src="../img/numeros.webp" class="botao" width="200px" ></button> <br>	
+		        <label for="cubo">4-7-11-18-29... Qual é o próximo número nessa sequência?</label> <br>
+		        
+		        <br>
+		        <button> <input type="text" placeholder="47"> </button> <br>
+		       	<button> <input type="text" placeholder="46"> </button> <br>
+		       	<button> <input type="text" placeholder="55"> </button> <br>
+		       	<button> <input type="text" placeholder="40"> </button> <br>
 		    </div>
 
 		</form>
