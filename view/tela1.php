6b39fcdad100b156e7133189264b4ad47e22f15b
karolinemagal <46010185+karolinemagal@users.noreply.github.com>
2019-11-19 20:47:58 -0300

    Update tela1.php

--- view/tela1.php
+++ view/tela1.php
@@ -5,61 +5,110 @@
 	</head>
 	<body>
 		<form action="/pagina-processa-dados-do-form" method="post">
+
 		    <div>
-		        <button><input type="image" src="../img/cubo1.webp" class="botao" width="200px" ></button> <br>	
-		        <label for="cubo">Qual dos cubos abaixo poderia ser a rotação do cubo acima?</label> <br>
-		        <label for="resposta1"> </label> <br>
-		    </div>
-		    <div>
-		        <button> <input type="image" src="../img/cubo2.webp" class="botao" width="100px" > </button> 
-		        <button> <input type="image" src="../img/cubo3.webp" class="botao" width="100px" > </button> 
-		        <button> <input type="image" src="../img/cubo4.webp" class="botao" width="100px" > </button> 
-		        <button> <input type="image" src="../img/cubo5.webp" class="botao" width="100px" > </button>
-		    </div>
-		    <hr>
-		    <br>
-		    <br>
-		    <br>
-		    <div>
-		        <button><input type="image" src="../img/calendario.webp" class="botao" width="200px" ></button> <br>	
-		        <label for="cubo">Se o dia depois de amanhã é dois dias antes da quinta-feira, que dia é hoje?</label> <br>
-		        <!--<label for="resposta1"> </label> <br>-->
-		        <br>
-		        <button> <input type="text" placeholder="Quarta"> </button> <br>
-		       	<button> <input type="text" placeholder="Segunda"> </button> <br>
-		       	<button> <input type="text" placeholder="Terça"> </button> <br>
-		       	<button> <input type="text" placeholder="Domingo"> </button> <br>
-		    </div>
-		    <hr>
-		    <br>
-		    <br>
-		    <br>
-		    <div>
-		        <button><input type="image" src="../img/cidade.webp" class="botao" width="200px" ></button> <br>	
-		        <label for="cubo">Marque a palavra que não combina com as outras</label> <br>
-		        <!--<label for="resposta1"> </label> <br>-->
-		        <br>
-		        <button> <input type="text" placeholder="Cuiabá"> </button> <br>
-		       	<button> <input type="text" placeholder="Cairo"> </button> <br>
-		       	<button> <input type="text" placeholder="Buenos Aires"> </button> <br>
-		       	<button> <input type="text" placeholder="Campinas"> </button> <br>
+		        <center>
+			        <button><input type="image" src="../img/cubo1.webp" class="botao" width="500px" ></button> <br>	
+			        <label for="cubo">Qual dos cubos abaixo poderia ser a rotação do cubo acima?</label> <br>
+			        <label for="resposta1"> </label> 
+			        <br>
+			        <button> <input type="image" src="../img/cubo2.webp" class="botao" width="100px" > </button> 
+			        <button> <input type="image" src="../img/cubo3.webp" class="botao" width="100px" > </button> 
+			        <button> <input type="image" src="../img/cubo4.webp" class="botao" width="100px" > </button> 
+			        <button> <input type="image" src="../img/cubo5.webp" class="botao" width="100px" > </button>
+		    	</center>
 		    </div>
 
 		    <hr>
 		    <br>
 		    <br>
 		    <br>
-		     <div>
-		        <button><input type="image" src="../img/numeros.webp" class="botao" width="200px" ></button> <br>	
-		        <label for="cubo">4-7-11-18-29... Qual é o próximo número nessa sequência?</label> <br>
+
+		    <div>
+		    	<center>
+			        <button><input type="image" src="../img/calendario.webp" class="botao" width="500px" ></button> <br>	
+			        <label for="cubo">Se o dia depois de amanhã é dois dias antes da quinta-feira, que dia é hoje?</label> <br>
+			        <!--<label for="resposta1"> </label> <br>-->
+			        <br>
+			        <button> <input type="text" placeholder="Quarta"> </button> <br>
+			       	<button> <input type="text" placeholder="Segunda"> </button> <br>
+			       	<button> <input type="text" placeholder="Terça"> </button> <br>
+			       	<button> <input type="text" placeholder="Domingo"> </button> <br>
+		    	</center>
+		    </div>
+
+		    <hr>
+		    <br>
+		    <br>
+		    <br>
+
+		    <div>
+		    	<center>
+			        <button><input type="image" src="../img/cidade.webp" class="botao" width="500px" ></button> <br>	
+			        <label for="cubo">Marque a palavra que não combina com as outras</label> <br>
+			        <!--<label for="resposta1"> </label> <br>-->
+			        <br>
+			        <button> <input type="text" placeholder="Cuiabá"> </button> <br>
+			       	<button> <input type="text" placeholder="Cairo"> </button> <br>
+			       	<button> <input type="text" placeholder="Buenos Aires"> </button> <br>
+			       	<button> <input type="text" placeholder="Campinas"> </button> <br>
+		    	</center>
+		    </div>
+
+		    <hr>
+		    <br>
+		    <br>
+		    <br>
+
+		    <div>
+		    	<center>
+			    	<button><input type="image" src="../img/numeros.webp" class="botao" width="500px" ></button> <br>	
+			        <label for="cubo">4-7-11-18-29... Qual é o próximo número nessa sequência?</label> <br>
+			        
+			        <br>
+			        <button> <input type="text" placeholder="47"> </button> <br>
+			       	<button> <input type="text" placeholder="46"> </button> <br>
+			       	<button> <input type="text" placeholder="55"> </button> <br>
+			       	<button> <input type="text" placeholder="40"> </button> <br>
+		    	</center>
+		    </div>
+			
+			<hr>
+		    <br>
+		    <br>
+		    <br>
+			
+			<div>
+				<center>
+					<button><input type="image" src="../img/pensar.webp" class="botao" width="500px" ></button> <br>	
+		        	<label for="cubo">Fernanda gosta do número 96, mas não do 45. Ela também gosta do número 540, mas não do 250. De qual número abaixo ela gosta?</label> <br>
 		        
-		        <br>
-		        <button> <input type="text" placeholder="47"> </button> <br>
-		       	<button> <input type="text" placeholder="46"> </button> <br>
-		       	<button> <input type="text" placeholder="55"> </button> <br>
-		       	<button> <input type="text" placeholder="40"> </button> <br>
+			        <br>
+			        <button> <input type="text" placeholder="128" > </button> <br>
+			       	<button> <input type="text" placeholder="140"> </button> <br>
+			       	<button> <input type="text" placeholder="93"> </button> <br>
+			       	<button> <input type="text" placeholder="132"> </button> <br>
+			    </center>
 		    </div>
-
+			
+			<hr>
+		    <br>
+		    <br>
+		    <br>
+			
+			<div>
+				<center>
+			    	<button><input type="image" src="../img/sequencia.webp" class="botao" width="500px" ></button> <br>	
+			        <label for="cubo">C, F, I, L, O... Nesta sequência, qual é a próxima letra?</label> <br>
+			        
+			        <br>
+			        <button> <input type="text" placeholder="T"> </button> <br>
+			       	<button> <input type="text" placeholder="R"> </button> <br>
+			       	<button> <input type="text" placeholder="W"> </button> <br>
+			       	<button> <input type="text" placeholder="Q"> </button> <br>
+		    	</center>
+		    </div>
+		     
 		</form>
 	</body>
 </html>
